1038d950c18ea2239920a2ed410fde59ec233327
Vicktorious23 <90080625+VICKTORIOUS23@users.noreply.github.com>
2024-11-09 18:10:50 -0600

    Rock Paper Scissors Step 2:
    - utilized Math.random to find a random number.
    - Assigned random number to rock paper or scissor.
    - Console print result.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,10 @@
 console.log("Hello World");
+
+let randcomp = Math.floor((Math.random())*2.9);
+let getComputerChoice = '';
+
+    if (randcomp == 0) {getComputerChoice = "Rock"};
+    if (randcomp == 1) {getComputerChoice = "Paper"};
+    if (randcomp == 2) {getComputerChoice = "Scissors"};
+
+console.log(getComputerChoice);
